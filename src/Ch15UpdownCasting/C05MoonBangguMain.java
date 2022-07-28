50f755800f08fbd5ca8aa4bc55d60c92140869ce
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-28 16:49:53 +0900

    220728

---
+++ src/Ch15UpdownCasting/C05MoonBangguMain.java
@@ -0,0 +1,54 @@
+package Ch15UpdownCasting;
+
+class Moonbanggu{
+	
+	static int scount = 100; // 샤프 재고량
+	static int pcount = 100; // 연필 재고량
+	
+	// 함수 정의
+	protected void write() {}
+	
+}
+class sharp extends Moonbanggu{
+	sharp(){
+		super.scount--;		// 샤프 객체 생성시 재고량 감소
+	}
+	protected void write() { System.out.println("샤프로 씁니다");}
+}
+class pencil extends Moonbanggu{
+	pencil(){
+		super.pcount--;		// 연필 객체 생성시 재고량 감소
+	}
+	protected void write() { System.out.println("연필로 씁니다");}
+}
+public class C05MoonBangguMain {
+	
+	public static Moonbanggu ReqMoonbanggu(String thing) {
+		if(thing.equals("샤프")) {
+			return new sharp();
+		}
+		else if(thing.equals("연필")) {
+			return new pencil();
+		}
+		else {
+			return null;
+		}
+		
+	}
+	
+	public static void ShowInfo() {
+		System.out.println("샤프 재고량 : "+ Moonbanggu.scount);
+		System.out.println("연필 재고량 : " + Moonbanggu.pcount);
+	}
+	public static void main(String[] args) {
+		sharp ob1 = (sharp)ReqMoonbanggu("샤프"); // sharp ob1 = 상위클래스 참조
+		sharp ob2 = (sharp)ReqMoonbanggu("샤프");
+		pencil ob3 = (pencil)ReqMoonbanggu("연필");
+		ob1.write();
+		ob2.write();
+		ob3.write();
+		ShowInfo();
+
+	}
+
+}
